919c364c28100c5c446e094f94b0e1db284605b1
mohit kushwaha <kushwahamohit000@gmail.com>
2022-03-23 17:29:13 +0530

    feat: added Home page

---
+++ src/pages/Home/Home.styles.js
@@ -0,0 +1,30 @@
+import { createUseStyles } from 'react-jss';
+import ImgSrc from '../../assets/background.png';
+
+export const useHomeStyles = createUseStyles(() => ({
+  background: {
+    alignItems: 'center',
+    backgroundImage: `url(${ImgSrc})`,
+    display: 'flex',
+    height: '90vh',
+    justifyContent: 'center',
+    width: '100%'
+  },
+  box: {
+    '&>p': {
+      fontSize: '1rem',
+      fontWeight: 'bold'
+    },
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: '0.7rem',
+    boxShadow: '0 4px 15px 3px rgb(0 0 0 / 25%)',
+    display: 'flex',
+    flexDirection: 'column',
+    height: '15.75rem',
+    justifyContent: 'space-evenly',
+    padding: '0.5rem',
+    textAlign: 'left',
+    width: '24.75rem'
+  }
+}));
