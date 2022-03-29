515a721743661b10e6f063e0ed873a1cec725c19
mohit kushwaha <kushwahamohit000@gmail.com>
2022-03-29 13:10:05 +0530

    chore: utility function to copy text to clipboard

--- src/utils/utility.js
+++ src/utils/utility.js
@@ -1,7 +1,10 @@
 export const BASE_URL = '';
 
 export const toSentenceCase = (s) => s[0].toUpperCase() + s.slice(1);
+
 export const parseNum = (val) => {
   const parsed = parseInt(val, 10);
   return isNaN(parsed) ? 0 : parsed;
 };
+
+export const copyToClipBoard = (text) => navigator.clipboard.writeText(text);
